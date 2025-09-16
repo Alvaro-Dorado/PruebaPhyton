aa76f5d89aff32e645ff3a7d76f60d61514b5d00
Alvaro-Dorado <dorado.cumbreras.alvaro@iescamas.es>
2025-09-16 20:40:03 +0200

    Add files via upload

---
+++ Lowest1.py
@@ -0,0 +1,12 @@
+#Esto es una funcion
+def lower_num(num1,num2):
+    if num1 <= num2:
+        lowest = num1
+    else:
+        lowest = num2
+
+#Esto es uan llamada a una funcion
+firt_num = int(input("Enter the first number:"))
+second_num = int(input("Enter the second number:"))
+
+print("The lowest number is " + str(lower_num(firt_num, second_num)))
